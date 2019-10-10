c4f70a4d9f8bbc089d20550ca0a70d958ba22921
markmead <itsmarkmead@gmail.com>
2019-10-10 11:18:17 +0100

    better error message

--- app/models/spina/inquiry.rb
+++ app/models/spina/inquiry.rb
@@ -2,8 +2,22 @@ module Spina
   class Inquiry < ActiveRecord::Base
     include ActionView::Helpers::TextHelper
 
-    validates_presence_of :email, :message, :name
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    validates :email,
+              presence: {
+                message: 'Enter email address'
+              },
+              format: {
+                with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                message: 'Enter a valid email address (e.g john@doe.com)'
+              }
+    validates :name,
+              presence: {
+                message: 'Enter name'
+              }
+    validates :message,
+              presence: {
+                message: 'Enter message'
+              }
 
     scope :new_messages, -> { where(archived: false) }
     scope :sorted, -> { order("created_at DESC") }
